a09992a445ce9501674d05b0e401bf24ce0c14c5
ajepe <ajepebabatope@gmail.com>
2019-03-19 16:13:45 +0100

    [IMP] clean reservation

--- hotel_reservation/models/__init__.py
+++ hotel_reservation/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import hotel_folio
 from . import reservation
+from . import reservation_summary
--- hotel_reservation/models/reservation.py
+++ hotel_reservation/models/reservation.py
@@ -57,6 +57,14 @@ class Reservation(models.Model):
 
     state = fields.Selection(selection=STATES, string='State', default='draft')
     folio_id = fields.Many2one('hotel.folio', string='Folio')
+    amount_total = fields.Float(string='Amount Total', compute='_compute_amount_total')
+    guest_company_id = fields.Many2one('res.partner', string='Company')
+
+    @api.depends('reserved_room_ids', 'night')
+    def _compute_amount_total(self):
+        for record in self:
+            self.amount_total = abs(sum([room.total_price for room in record.reserved_room_ids]) * record.night)
+
 
     @api.depends('number_of_rooms')
     def _compute_room_reservation(self):
@@ -221,81 +229,3 @@ class Reservation(models.Model):
         vals.update(name=self.env['ir.sequence'].next_by_code(
             'hotel.reservation'))
         return super(Reservation, self).create(vals)
-
-
-class ReservationSummary(models.Model):
-
-    _name = 'room.reservation.summary'
-    _description = 'Room reservation summary'
-
-    name = fields.Char(string='Reservation Summary', default='Reservations Summary',
-                       invisible=True)
-    date_from = fields.Datetime(string='Date From')
-    date_to = fields.Datetime(string='Date To')
-    summary_header = fields.Text(string='Summary Header')
-    room_summary = fields.Text(string='Room Summary')
-
-    def list_date_between(self, date_from, date_to):
-        date_format = ['Room', 'Jan-1-2018',
-                       'Jan-2-2018', 'Jan-3-2018', 'Jan-4-2018']
-        return date_format
-
-    @api.model
-    def default_get(self, fields):
-        """
-        To get default values for the object.
-        @param self: The object pointer.
-        @param fields: List of fields for which we want default values
-        @return: A dictionary which of fields with values.
-        """
-        res = super(ReservationSummary, self).default_get(fields)
-        # Added default datetime as today and date to as today + 30.
-        from_dt = datetime.today()
-        dt_from = from_dt.strftime(dt)
-        to_dt = from_dt + relativedelta(days=30)
-        dt_to = to_dt.strftime(dt)
-        date_between = self.list_date_between(dt_from, dt_to)
-        res.update({'date_from': dt_from, 'date_to': dt_to,
-                    'summary_header': date_between})
-
-        if not self.date_from and self.date_to:
-            date_today = datetime.datetime.today()
-            first_day = datetime.datetime(date_today.year,
-                                          date_today.month, 1, 0, 0, 0)
-            first_temp_day = first_day + relativedelta(months=1)
-            last_temp_day = first_temp_day - relativedelta(days=1)
-            last_day = datetime.datetime(last_temp_day.year,
-                                         last_temp_day.month,
-                                         last_temp_day.day, 23, 59, 59)
-            date_froms = first_day.strftime(dt)
-            date_ends = last_day.strftime(dt)
-            res.update({'date_from': date_froms, 'date_to': date_ends})
-        return res
-
-    @api.multi
-    def room_reservation(self):
-        """
-        @param self: object pointer
-        """
-        mod_obj = self.env['ir.model.data']
-        if self._context is None:
-            self._context = {}
-        model_data_ids = mod_obj.search([('model', '=', 'ir.ui.view'),
-                                         ('name', '=',
-                                          'view_hotel_reservation_form')])
-        resource_id = model_data_ids.read(fields=['res_id'])[0]['res_id']
-        return {'name': _('Reconcile Write-Off'),
-                'context': self._context,
-                'view_type': 'form',
-                'view_mode': 'form',
-                'res_model': 'hotel.reservation',
-                'views': [(resource_id, 'form')],
-                'type': 'ir.actions.act_window',
-                'target': 'new',
-                }
-
-    @api.onchange('date_from', 'date_to')
-    def get_room_summary(self):
-        import random
-        a = random.randrange(1, 20)
-        self.summary_header = list(range(1, a))
